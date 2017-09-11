d4959737c0d7cda731108b6e1d19edf0c368357a
lizcw <e.cooperwilliams@uq.edu.au>
2017-09-11 18:11:22 +1000

    Ratios added

--- batchHistogramStats.py
+++ batchHistogramStats.py
@@ -40,9 +40,11 @@ class HistoStats():
         self.outputdir = outputdir
         if len(prefix) > 0:
             prefix = prefix + "_"
+        self.prefix = prefix
         self.compiledfile = join(outputdir,prefix + datafile)
         self.compiled = pd.DataFrame()
         self.histofile = 'Histogram_log10D.csv'
+        self.numcells = 0
 
     def compile(self):
         #get list of files to compile
@@ -52,6 +54,7 @@ class HistoStats():
             c = pd.DataFrame()
             base = self.inputdir.split(sep)
             suffixes = []
+            self.numcells = len(result)
             for f in result:
                 df = pd.read_csv(f)
                 cell = f.split(sep)
@@ -68,25 +71,69 @@ class HistoStats():
     def runStats(self):
         print("Running stats")
         df = self.compiled
-        df['Total_sum'] = df.apply(lambda x: (x[1:].sum()),axis=1)
-        df['Total_count'] = df.apply(lambda x: (x[1:-1].count()), axis=1)
-        df['Total_mean'] = df.apply(lambda x: (x[1:-2].mean()), axis=1)
-        df['Total_std'] = df.apply(lambda x: (x[1:-3].std()), axis=1)
-        df['Total_sem'] = df.apply(lambda x: (x.loc['Total_std']/np.sqrt(x.loc['Total_count'])), axis=1)
+        #ORDER: mean,sem,count,std,sum
+        df['SUM'] = df.apply(lambda x: (x[1:].sum()),axis=1)
+        df['COUNT'] = df.apply(lambda x: (x[1:-1].count()), axis=1)
+        df['MEAN'] = df.apply(lambda x: (x[1:-2].mean()), axis=1)
+        df['STD'] = df.apply(lambda x: (x[1:-3].std()), axis=1)
+        df['SEM'] = df.apply(lambda x: (x.loc['STD']/np.sqrt(x.loc['COUNT'])), axis=1)
         df.to_csv(self.compiledfile, index=False)
         self.compiled = df
 
-    def showPlots(self):
-        print("Show plots")
+    def splitMobile(self, threshold):
+        print("Split mobile and immobile fractions")
         df = self.compiled
-        fig, ax = plt.subplots()
+        labels =[]
+        immobile =[]
+        mobile =[]
+        ratio = []
+        try:
+            if type(threshold) != 'float':
+                threshold = float(threshold)
+            for i in range(1,self.numcells + 1):
+                label = df.columns[i]
+                labels.append(label)
+                immobile.append(df[label][df['bins'] <= threshold].sum())
+                mobile.append(df[label][df['bins'] > threshold].sum())
+
+            df_results = pd.DataFrame({'Cell': labels, 'Immobile': immobile, 'Mobile': mobile})
+            df_results['Ratio'] = df_results['Immobile']/ df_results['Mobile']
+            ratiofile = join(self.outputdir,self.prefix + "_ratios.csv")
+            df_results.to_csv(ratiofile)
+            print("Output ratios:", ratiofile)
+
+        except ValueError as e:
+            print("Error:", e)
+
+
+    def showPlots(self, ax=None,threshold=False):
+        title = "Individual cells"
+        df = self.compiled
+        if ax is None:
+            fig, ax = plt.subplots()
         labels=[]
         for i in range(1,len(df.columns)-5):
-            df.plot.line('bins',i,ax=ax)
+            df.plot.line('bins', i, ax=ax)
             labels.append(df.columns[i])
+        #plot threshold line
+        if threshold:
+            plt.axvline(x=-1.6, color='r', linestyle='-', linewidth=0.5)
         lines, _ = ax.get_legend_handles_labels()
         ax.legend(lines, labels, loc='best')
-        plt.show()
+        plt.title(title)
+
+
+    def showAvgPlot(self, ax=None):
+        print("Show average plots for treatments")
+        df = self.compiled
+        width=0.15
+        if ax is None:
+            fig, ax = plt.subplots()
+        plt.bar(df['bins'],df['MEAN'],yerr=df['SEM'], width=width)
+        #df['Total_mean'].plot.bar(yerr=df['Total_sem'])
+        plt.xlabel('bins')
+        plt.title('Average with SEM')
+
 
 #################################################################################################
 if __name__ == "__main__":
@@ -100,7 +147,7 @@ if __name__ == "__main__":
     parser.add_argument('--datafile', action='store', help='Initial data file - D', default="AllHistogram_log10D.csv")
     parser.add_argument('--outputdir', action='store', help='Output directory', default="data")
     parser.add_argument('--prefix', action='store', help='Prefix for compiled file eg STIM or NOSTIM', default="")
-
+    parser.add_argument('--threshold', action='store', help='Threshold between mobile and immobile', default="-1.6")
     args = parser.parse_args()
 
     print("Loading Input from :", args.filedir)
@@ -109,6 +156,17 @@ if __name__ == "__main__":
         fmsd = HistoStats(args.filedir,args.outputdir,args.datafile,args.prefix)
         fmsd.compile()
         fmsd.runStats()
-        fmsd.showPlots()
+        # Split to Mobile/immobile fractions - output
+        fmsd.splitMobile(float(args.threshold))
+        # Set the figure
+        #fig = plt.figure(figsize=(10, 5))
+        #axes1 = plt.subplot(121)
+        #fmsd.showPlots(axes1,False)
+
+        #axes2 = plt.subplot(122)
+        #fmsd.showAvgPlot(axes2)
+        #plt.show()
+
+
     except ValueError as e:
         print("Error: ", e)
